9de52b6dc6db49d24c0d3b416074f95c29dc218e
Slavik Baranov <slavik.baranov@gmail.com>
2023-06-23 20:19:33 +0300

    Simplify IPFS config serialization

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -4,56 +4,72 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 #[serde(rename_all = "snake_case")]
-pub enum IpfsLoaderConfig {
-    HttpGateway { url: String },
-    IpfsNode { url: String },
+pub enum IpfsConnectionType {
+    HttpGateway,
+    IpfsNode,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[serde(rename_all = "snake_case")]
+pub struct IpfsLoaderConfig {
+    connection_type: IpfsConnectionType,
+    base_url: String,
 }
 
 impl IpfsLoaderConfig {
     pub fn http_gateway(url: &str) -> IpfsLoaderConfig {
-        IpfsLoaderConfig::HttpGateway {
-            url: url.to_string(),
+        IpfsLoaderConfig {
+            connection_type: IpfsConnectionType::HttpGateway,
+            base_url: url.to_string(),
         }
     }
 
     pub fn ipfs_node(url: &str) -> IpfsLoaderConfig {
-        IpfsLoaderConfig::IpfsNode {
-            url: url.to_string(),
+        IpfsLoaderConfig {
+            connection_type: IpfsConnectionType::IpfsNode,
+            base_url: url.to_string(),
         }
     }
 }
 
 impl Default for IpfsLoaderConfig {
     fn default() -> Self {
-        Self::HttpGateway {
-            url: "https://cloudflare-ipfs.com/ipfs/".to_string(),
+        Self {
+            connection_type: IpfsConnectionType::HttpGateway,
+            base_url: "https://cloudflare-ipfs.com/ipfs/".to_string(),
         }
     }
 }
 
 #[derive(Clone)]
 pub struct IpfsLoader {
-    config: IpfsLoaderConfig,
+    connection_type: IpfsConnectionType,
+    base_url: String,
     client: reqwest::Client,
 }
 
 impl IpfsLoader {
     pub fn new(config: &IpfsLoaderConfig) -> anyhow::Result<Self> {
         Ok(Self {
-            config: config.clone(),
+            connection_type: config.connection_type.clone(),
+            base_url: config.base_url.clone(),
             client: reqwest::Client::builder().build()?,
         })
     }
 
+    pub fn default() -> anyhow::Result<Self> {
+        Self::new(&IpfsLoaderConfig::default())
+    }
+
     pub async fn load(&self, path: &str) -> anyhow::Result<Vec<u8>> {
-        let response = match &self.config {
-            IpfsLoaderConfig::HttpGateway { url } => {
-                let uri = format!("{}/{}", url, path);
-                self.client.get(uri).send().await?
+        let response = match self.connection_type {
+            IpfsConnectionType::HttpGateway => {
+                let full_url = format!("{}/{}", self.base_url, path);
+                self.client.get(full_url).send().await?
             }
-            IpfsLoaderConfig::IpfsNode { url } => {
-                let uri = format!("{}/api/v0/cat?arg={}", url, path);
-                self.client.post(uri).send().await?
+            IpfsConnectionType::IpfsNode => {
+                let full_url = format!("{}/api/v0/cat?arg={}", self.base_url, path);
+                self.client.post(full_url).send().await?
             }
         };
         let status = response.status();
@@ -82,35 +98,24 @@ impl IpfsLoader {
         let str = String::from_utf8(bytes)?;
         Ok(str)
     }
-
-    pub fn default() -> anyhow::Result<Self> {
-        Self::new(&IpfsLoaderConfig::default())
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::ipfs::IpfsLoaderConfig;
+    use crate::ipfs::{IpfsConnectionType, IpfsLoaderConfig};
 
     static CONFIG_JSON: &str = r#"
     {
-      "http_gateway": {
-        "url": "http://example.com/"
-      }
+      "connection_type": "http_gateway",
+      "base_url": "http://example.com/"
     }
     "#;
 
     #[test]
     fn test_config_deserialization() -> anyhow::Result<()> {
         let config: IpfsLoaderConfig = serde_json::from_str(CONFIG_JSON)?;
-        match config {
-            IpfsLoaderConfig::HttpGateway { url } => {
-                assert_eq!(url, "http://example.com/");
-            }
-            _ => {
-                panic!("Expected HttpGateway config, got {:?}", config);
-            }
-        };
+        assert_eq!(config.connection_type, IpfsConnectionType::HttpGateway);
+        assert_eq!(config.base_url, "http://example.com/");
         Ok(())
     }
 }
