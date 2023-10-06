3b42d7a389ac93c4ffc4f5e1a9e501247cb2001f
Dmitrii Baranov <d.baranov@ston.fi>
2023-10-06 14:45:31 +0000

    added to_short_string method to tonresult

--- src/tl/result.rs
+++ src/tl/result.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use strum::{Display, EnumDiscriminants, IntoStaticStr};
 
 use crate::tl::types::{
@@ -9,16 +10,7 @@ use crate::tl::types::{
 use crate::{client::TonClientError, tl::stack::TvmCell};
 
 #[derive(
-    IntoStaticStr,
-    EnumDiscriminants,
-    Serialize,
-    Deserialize,
-    Debug,
-    Clone,
-    PartialEq,
-    Eq,
-    Hash,
-    Display,
+    IntoStaticStr, EnumDiscriminants, Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash,
 )]
 #[strum_discriminants(derive(IntoStaticStr, Display))]
 #[serde(tag = "@type", rename_all = "camelCase")]
@@ -91,6 +83,97 @@ impl TonResult {
                 TonResultDiscriminants::Ok,
                 r.clone(),
             )),
+        }
+    }
+}
+impl fmt::Display for TonResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            // tonlib_api.tl, line 20
+            TonResult::Error { code, message } => {
+                write!(f, "TonResult::Error {}: {} ", code, message)
+            }
+
+            TonResult::Ok {} => write!(f, "TonResult::Ok"),
+
+            TonResult::OptionsInfo(options_info) => write!(
+                f,
+                "TonResult::OptionsInfo: {}",
+                options_info.config_info.default_wallet_id
+            ),
+
+            TonResult::BlockIdExt(block_id_ext) => write!(
+                f,
+                "TonResult::BlockIdExt: {}:{}, seqno{}",
+                block_id_ext.workchain, block_id_ext.shard, block_id_ext.seqno
+            ),
+
+            TonResult::RawFullAccountState(raw_full_account_state) => write!(
+                f,
+                "TonResult::RawFullAccountState: {}:{}, seqno{}, last_td_id {}",
+                raw_full_account_state.block_id.workchain,
+                raw_full_account_state.block_id.shard,
+                raw_full_account_state.block_id.seqno,
+                raw_full_account_state.last_transaction_id
+            ),
+
+            TonResult::RawTransactions(raw_transactions) => write!(
+                f,
+                "TonResult::RawTransactions: prev_tx_id {}",
+                raw_transactions.previous_transaction_id
+            ),
+
+            TonResult::RawExtMessageInfo(_) => write!(f, "TonResult::RawExtMessageInfo"),
+
+            TonResult::FullAccountState(full_account_state) => write!(
+                f,
+                "TonResult::FullAccountState: address: {}",
+                full_account_state.address.account_address
+            ),
+
+            TonResult::SmcInfo(_) => write!(f, "TonResult::SmcInfo"),
+
+            TonResult::SmcRunResult(smc_run_result) => {
+                write!(f, "TonResult::SmcRunResult: {}", smc_run_result.exit_code)
+            }
+
+            TonResult::UpdateSyncState(_) => write!(f, "TonResult::UpdateSyncState"),
+
+            TonResult::LiteServerInfo(_) => write!(f, "TonResult::LiteServerInfo"),
+
+            TonResult::LogVerbosityLevel(log_verbosity_level) => write!(
+                f,
+                "TonResult::LogVerbosityLevel: {}",
+                log_verbosity_level.verbosity_level
+            ),
+
+            TonResult::BlocksMasterchainInfo(blocks_masterchain_info) => write!(
+                f,
+                "TonResult::BlocksMasterchainInfo: {}:{}, seqno{}",
+                blocks_masterchain_info.last.workchain,
+                blocks_masterchain_info.last.shard,
+                blocks_masterchain_info.last.seqno
+            ),
+
+            TonResult::BlocksShards(_) => write!(f, "TonResult::BlocksShards"),
+
+            TonResult::BlocksTransactions(blocks_trasnactions) => write!(
+                f,
+                "TonResult::BlocksTransactions: {}:{}, seqno{}",
+                blocks_trasnactions.id.workchain,
+                blocks_trasnactions.id.shard,
+                blocks_trasnactions.id.seqno
+            ),
+
+            TonResult::BlocksHeader(blocks_header) => write!(
+                f,
+                "TonResult::BlocksHeader: {}:{}, seqno{}",
+                blocks_header.id.workchain, blocks_header.id.shard, blocks_header.id.seqno
+            ),
+
+            TonResult::ConfigInfo(_) => write!(f, "TonResult::ConfigInfo"),
+
+            TonResult::TvmCell(_) => write!(f, "TonResult::TvmCell"),
         }
     }
 }
