0bd7e053b5e16e4725941c3a511d1df330096aa7
Dmitrii Baranov <d.baranov@ston.fi>
2024-02-13 13:10:16 +0000

    Impl #28: State init extension

--- src/cell/state_init.rs
+++ src/cell/state_init.rs
@@ -2,27 +2,52 @@ use std::sync::Arc;
 
 use crate::cell::{Cell, CellBuilder, TonCellError};
 
+pub struct StateInitBuilder {
+    code: Option<Arc<Cell>>,
+    data: Option<Arc<Cell>>,
+    split_depth: bool,
+    tick_tock: bool,
+    library: bool,
+}
 pub struct StateInit {
     pub code: Option<Arc<Cell>>,
     pub data: Option<Arc<Cell>>,
 }
 
-impl StateInit {
-    pub fn new(code: &Arc<Cell>, data: &Arc<Cell>) -> StateInit {
-        StateInit {
+impl StateInitBuilder {
+    pub fn new(code: &Arc<Cell>, data: &Arc<Cell>) -> StateInitBuilder {
+        StateInitBuilder {
             code: Some(code.clone()),
             data: Some(data.clone()),
+            split_depth: false,
+            tick_tock: false,
+            library: false,
         }
+    }
+
+    pub fn with_split_depth(&mut self, split_depth: bool) -> &mut Self {
+        self.split_depth = split_depth;
+        self
+    }
+
+    pub fn with_tick_tock(&mut self, tick_tock: bool) -> &mut Self {
+        self.tick_tock = tick_tock;
+        self
+    }
+
+    pub fn with_library(&mut self, library: bool) -> &mut Self {
+        self.library = library;
+        self
     }
 
     pub fn build(&self) -> Result<Cell, TonCellError> {
         let mut builder = CellBuilder::new();
         builder
-            .store_bit(false)? //Split depth
-            .store_bit(false)? //Tick tock (always false)
+            .store_bit(self.split_depth)? //Split depth
+            .store_bit(self.tick_tock)? //Tick tock
             .store_bit(self.code.is_some())? //Code
             .store_bit(self.data.is_some())? //Data
-            .store_bit(false)?; //Library
+            .store_bit(self.library)?; //Library
         if let Some(code) = &self.code {
             builder.store_reference(code)?;
         }
@@ -31,8 +56,72 @@ impl StateInit {
         }
         builder.build()
     }
+}
 
+impl StateInit {
     pub fn create_account_id(code: &Arc<Cell>, data: &Arc<Cell>) -> Result<Vec<u8>, TonCellError> {
-        Self::new(code, data).build()?.cell_hash()
+        StateInitBuilder::new(code, data).build()?.cell_hash()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use crate::cell::CellBuilder;
+
+    use super::StateInitBuilder;
+
+    #[test]
+    fn test_state_init() -> anyhow::Result<()> {
+        let code = Arc::new(CellBuilder::new().store_string("code")?.build()?);
+        let data = Arc::new(CellBuilder::new().store_string("data")?.build()?);
+        let state_init = StateInitBuilder::new(&code, &data)
+            .with_split_depth(true)
+            .with_tick_tock(true)
+            .with_library(true)
+            .build()?;
+
+        assert_eq!(state_init.data[0], 0b11111000);
+        println!("{:08b}", state_init.data[0]);
+
+        let code = Arc::new(CellBuilder::new().store_string("code")?.build()?);
+        let data = Arc::new(CellBuilder::new().store_string("data")?.build()?);
+        let state_init = StateInitBuilder::new(&code, &data)
+            .with_split_depth(false)
+            .with_tick_tock(false)
+            .with_library(false)
+            .build()?;
+
+        assert_eq!(state_init.data[0], 0b00110000);
+
+        let code = Arc::new(CellBuilder::new().store_string("code")?.build()?);
+        let data = Arc::new(CellBuilder::new().store_string("data")?.build()?);
+        let state_init = StateInitBuilder::new(&code, &data)
+            .with_split_depth(true)
+            .with_tick_tock(false)
+            .with_library(false)
+            .build()?;
+
+        assert_eq!(state_init.data[0], 0b10110000);
+
+        let code = Arc::new(CellBuilder::new().store_string("code")?.build()?);
+        let data = Arc::new(CellBuilder::new().store_string("data")?.build()?);
+        let state_init = StateInitBuilder::new(&code, &data)
+            .with_split_depth(false)
+            .with_tick_tock(true)
+            .with_library(false)
+            .build()?;
+        assert_eq!(state_init.data[0], 0b01110000);
+
+        let code = Arc::new(CellBuilder::new().store_string("code")?.build()?);
+        let data = Arc::new(CellBuilder::new().store_string("data")?.build()?);
+        let state_init = StateInitBuilder::new(&code, &data)
+            .with_split_depth(false)
+            .with_tick_tock(false)
+            .with_library(true)
+            .build()?;
+        assert_eq!(state_init.data[0], 0b00111000);
+        Ok(())
     }
 }
