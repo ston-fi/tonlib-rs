684f7cd7f62d236cf888a128c35d7dd9c82f3d86
dbaranov34 <baranov34@gmail.com>
2024-03-27 00:28:42 +0400

    fix of connection test

--- tests/connection_test.rs
+++ tests/connection_test.rs
@@ -5,7 +5,7 @@ use std::time::Duration;
 use tonlib::address::TonAddress;
 use tonlib::client::{
     MultiConnectionCallback, TonClientError, TonClientInterface, TonConnection,
-    TonConnectionCallback, DEFAULT_CONNECTION_LIMIT, DEFAULT_CONNECTION_PARAMS,
+    TonConnectionCallback, TonConnectionParams, DEFAULT_CONNECTION_PARAMS,
     LOGGING_CONNECTION_CALLBACK, NOOP_CONNECTION_CALLBACK,
 };
 use tonlib::tl::{
@@ -19,7 +19,7 @@ async fn test_connection_init() -> anyhow::Result<()> {
     common::init_logging();
     let conn = TonConnection::new(
         LOGGING_CONNECTION_CALLBACK.clone(),
-        Some(DEFAULT_CONNECTION_LIMIT),
+        &TonConnectionParams::default(),
     )?;
     let r = conn
         .init(
@@ -89,7 +89,7 @@ async fn test_connection_callback() -> anyhow::Result<()> {
         test_callback.clone(),
         LOGGING_CONNECTION_CALLBACK.clone(),
     ]));
-    let conn = TonConnection::new(multi_callback, Some(DEFAULT_CONNECTION_LIMIT))?;
+    let conn = TonConnection::new(multi_callback, &TonConnectionParams::default())?;
     let r = conn
         .init(
             tonlib::config::MAINNET_CONFIG,
