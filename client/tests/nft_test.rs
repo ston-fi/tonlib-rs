5d2dcb528be690cd0b227f2e511c7e06d4f09ac0
Mikhail Zverev <m.u.zverev@gmail.com>
2024-12-10 18:15:52 +0400

    vip: finished with test for BC

--- client/tests/nft_test.rs
+++ client/tests/nft_test.rs
@@ -3,7 +3,7 @@ use sha2::{Digest, Sha256};
 use std::str::FromStr;
 use tokio_test::assert_ok;
 use tonlib_client::contract::{
-    NftCollectionContract, NftItemContract, NftItemData, TonContractFactory,
+    NftCollectionContract, NftCollectionData, NftItemContract, NftItemData, TonContractFactory,
 };
 use tonlib_client::meta::MetaDataContent::External;
 use tonlib_client::meta::{LoadMeta, MetaDataContent, NftColletionMetaLoader, NftItemMetaLoader};
@@ -11,7 +11,7 @@ use tonlib_core::{TonAddress, TonHash};
 
 mod common;
 
-// ---- A group of tests that the methods basically work
+// ---- Tests for basic check methods
 #[tokio::test]
 async fn test_get_nft_data() {
     common::init_logging();
@@ -46,7 +46,7 @@ async fn test_get_nft_address_by_index() -> anyhow::Result<()> {
     Ok(())
 }
 
-// ---- A group of tests that methods return valid data
+// ---- Tests for check is valid data
 #[tokio::test]
 async fn test_get_nft_data_is_valid() -> anyhow::Result<()> {
     common::init_logging();
@@ -56,7 +56,6 @@ async fn test_get_nft_data_is_valid() -> anyhow::Result<()> {
         "EQCGZEZZcYO9DK877fJSIEpYMSvfui7zmTXGhq0yq1Ce1Mb6".parse()
     ));
     let res = assert_ok!(contract.get_nft_data().await);
-    log::info!("{:#?}", res);
 
     let expected_collection_address = assert_ok!(TonAddress::from_base64_url(
         &"EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N".to_string()
@@ -67,7 +66,6 @@ async fn test_get_nft_data_is_valid() -> anyhow::Result<()> {
     let expected_index = assert_ok!(BigUint::from_str(
         "15995005474673311991943775795727481451058346239240361725119718297821926435889",
     ));
-    println!("{:?}", expected_collection_address);
     let expected_response = NftItemData {
         init: true,
         index: expected_index,
@@ -82,69 +80,7 @@ async fn test_get_nft_data_is_valid() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-async fn test_get_nft_content_uri() -> anyhow::Result<()> {
-    common::init_logging();
-    let client = common::new_mainnet_client().await;
-    let factory = TonContractFactory::builder(&client).build().await?;
-    let contract = factory.get_contract(&assert_ok!(
-        "EQCGZEZZcYO9DK877fJSIEpYMSvfui7zmTXGhq0yq1Ce1Mb6".parse()
-    ));
-    let res = assert_ok!(contract.get_nft_data().await);
-
-    let expected_uri = "https://nft.fragment.com/number/88805397120.json".to_string();
-    assert_eq!(
-        res.individual_content,
-        MetaDataContent::External { uri: expected_uri }
-    );
-    Ok(())
-}
-
-#[tokio::test]
-async fn test_get_nft_content_arkenston() -> anyhow::Result<()> {
-    common::init_logging();
-    let client = common::new_mainnet_client().await;
-    let factory = assert_ok!(TonContractFactory::builder(&client).build().await);
-    let contract = factory.get_contract(&assert_ok!(
-        "EQDhR36C8pSVtyhOFtE9nh2DFq4WYUbTZFmvjfnShlrXq2cz".parse()
-    ));
-    let res = assert_ok!(contract.get_nft_data().await);
-
-    // Пилить ниже
-    let meta_loader = assert_ok!(NftItemMetaLoader::default());
-    let content_res = assert_ok!(meta_loader.load(&res.individual_content).await);
-    assert_eq!(
-        content_res.image.unwrap(),
-        "https://static.ston.fi/stake-nft/i4.jpg"
-    );
-    assert_eq!(content_res.name.unwrap(), "ARKENSTON NFT");
-    Ok(())
-}
-
-#[tokio::test]
-async fn test_get_nft_content_some() -> anyhow::Result<()> {
-    common::init_logging();
-    let client = common::new_mainnet_client().await;
-    let factory = TonContractFactory::builder(&client).build().await?;
-    let contract = factory.get_contract(&assert_ok!(
-        "EQCiXgoveScGKGGqo50HbmwP3goKJaEfu9QmeBRJ-jbRxM21".parse()
-    ));
-    let res = assert_ok!(contract.get_nft_data().await);
-
-    // Пилить здесь.
-    let meta_loader = assert_ok!(NftItemMetaLoader::default());
-    let content_res = assert_ok!(meta_loader.load(&res.individual_content).await);
-    assert_eq!(
-        content_res.image.unwrap(),
-        "https://mars.tonplanets.com/i/biomes/4v4.jpg"
-    );
-    assert_eq!(content_res.name.unwrap(), "Anda");
-    Ok(())
-}
-
-// ---------------------nft get collection metadata tests
-
-#[tokio::test]
-async fn test_get_nft_collection_content_uri() -> anyhow::Result<()> {
+async fn test_get_nft_collection_data_is_valid() -> anyhow::Result<()> {
     common::init_logging();
     let client = common::new_archive_mainnet_client().await;
     let factory = TonContractFactory::builder(&client).build().await?;
@@ -153,26 +89,49 @@ async fn test_get_nft_collection_content_uri() -> anyhow::Result<()> {
     ));
     let res = assert_ok!(contract.get_collection_data().await);
 
-    assert_eq!(
-        res.collection_content,
-        MetaDataContent::External {
-            uri: "https://nft.fragment.com/numbers.json".to_string()
-        }
-    );
-
-    let meta_loader = NftColletionMetaLoader::default()?;
-    let content_res = assert_ok!(meta_loader.load(&res.collection_content).await);
-    assert_eq!(
-        content_res.name.as_ref().unwrap(),
-        &String::from("Anonymous Telegram Numbers")
-    );
-    assert_eq!(
-        content_res.image.as_ref().unwrap(),
-        &String::from("https://nft.fragment.com/numbers.svg")
-    );
+    let expected_owner_address = assert_ok!(TonAddress::from_base64_url(
+        &"EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c".to_string()
+    ));
+    let expected_response = NftCollectionData {
+        next_item_index: -1,
+        owner_address: expected_owner_address,
+        collection_content: External {
+            uri: "https://nft.fragment.com/numbers.json".to_string(),
+        },
+    };
+    assert_eq!(res, expected_response);
     Ok(())
 }
 
+#[tokio::test]
+async fn test_get_nft_address_by_index_is_valid() -> anyhow::Result<()> {
+    common::init_logging();
+    let client = common::new_mainnet_client().await;
+    let factory = TonContractFactory::builder(&client).build().await?;
+    let contract = factory.get_contract(&assert_ok!(
+        "EQB2iHQ9lmJ9zvYPauxN9hVOfHL3c_fuN5AyRq5Pm84UH6jC".parse()
+    ));
+
+    let res_0 = assert_ok!(contract.get_nft_address_by_index(0).await);
+    let res_2 = assert_ok!(contract.get_nft_address_by_index(2).await);
+    let res_1 = assert_ok!(contract.get_nft_address_by_index(1).await);
+
+    let expected_addr_0 = assert_ok!(TonAddress::from_base64_url(
+        &"EQBKwtMZSZurMxGp7FLZ_lM9t54_ECEsS46NLR3qfIwwTnKW".to_string()
+    ));
+    let expected_addr_1 = assert_ok!(TonAddress::from_base64_url(
+        &"EQB6rnPIZr8dXmLy0xVp4lTe1AlYRwOUghEG9zzCcCkCp8IS".to_string()
+    ));
+    let expected_addr_2 = assert_ok!(TonAddress::from_base64_url(
+        &"EQD0VQNu41wZmWMQjXfifnljGR0vOAULh0stBLItskMavwH0".to_string()
+    ));
+    assert_eq!(res_0, expected_addr_0);
+    assert_eq!(res_1, expected_addr_1);
+    assert_eq!(res_2, expected_addr_2);
+    Ok(())
+}
+
+// ------------ Checked before this line
 #[tokio::test]
 async fn test_get_nft_collection_content_arkenston() -> anyhow::Result<()> {
     common::init_logging();
