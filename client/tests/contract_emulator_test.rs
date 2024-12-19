d7f8d7a33b86e3ee69d2e1c866901e2395ba40c0
Dmitrii Baranov <d.baranov@ston.fi>
2024-12-19 10:04:15 +0000

    Impl #BE-1843: Bencmark for contract emulator

--- client/tests/contract_emulator_test.rs
+++ client/tests/contract_emulator_test.rs
@@ -1,14 +1,16 @@
 mod common;
+use std::time::{Duration, Instant};
+
 use tokio_test::assert_ok;
 use tonlib_client::contract::{
-    JettonData, JettonMasterContract, TonContractFactory, TonContractInterface,
+    JettonData, JettonMasterContract, TonContractError, TonContractFactory, TonContractInterface,
 };
 use tonlib_client::emulator::c7_register::TvmEmulatorC7;
 use tonlib_client::emulator::tvm_emulator::TvmEmulator;
 use tonlib_client::meta::MetaDataContent;
 use tonlib_client::tl::RawFullAccountState;
-use tonlib_client::types::TvmStackEntry;
-use tonlib_core::cell::{CellBuilder, CellSlice};
+use tonlib_client::types::{TonMethodId, TvmStackEntry, TvmSuccess};
+use tonlib_core::cell::{BagOfCells, CellBuilder, CellSlice};
 use tonlib_core::{TonAddress, TonTxId};
 
 #[tokio::test]
@@ -193,5 +195,138 @@ async fn test_emulate_ston_router_v2() -> anyhow::Result<()> {
             }
         }
     }
+    Ok(())
+}
+
+/// Benchmark for emulator
+#[ignore]
+#[tokio::test]
+async fn benchmark_emulate_ston_router_v2() -> anyhow::Result<()> {
+    common::init_logging();
+    let client = common::new_archive_mainnet_client().await;
+    let factory = TonContractFactory::builder(&client).build().await?;
+
+    let router_address = "EQCqX53C_Th32Xg7UyrlqF0ypmePjljxG8edlwfT-1QpG3TB".parse()?;
+    let tx_id = TonTxId::from_lt_hash(
+        51600010000005,
+        "82218cf8373437ffeac1bf306f44d9638894c2d2b4b2bddf85ac2c571b56b2a7",
+    )?;
+
+    let contract = factory.get_contract(&router_address);
+    let state = contract.get_state_by_transaction(&tx_id.into()).await?;
+
+    let token1_addr = CellSlice::full_cell(
+        CellBuilder::new()
+            .store_address(&"EQC8JhkQsgAwRpe0lMsr6U11NXWjwgty22gxnRt_pSq4jDmb".parse()?)?
+            .build()?,
+    )?;
+    let token2_addr = CellSlice::full_cell(
+        CellBuilder::new()
+            .store_address(&"EQB1R5vBgbJBZNVkh55XID629E2Xq9MFib3nai9QSkZ2F7X4".parse()?)?
+            .build()?,
+    )?;
+
+    let stack = &vec![
+        TvmStackEntry::Slice(token1_addr.clone()),
+        TvmStackEntry::Slice(token2_addr.clone()),
+    ];
+    let stack_ref: &[TvmStackEntry] = stack.as_ref();
+
+    let code = state.get_account_state().code.clone();
+    let data = state.get_account_state().data.clone();
+
+    let code_cell = BagOfCells::parse(&code)?.into_single_root()?;
+    let data_cell = BagOfCells::parse(&data)?.into_single_root()?;
+
+    let c7 = TvmEmulatorC7::new(
+        router_address.clone(),
+        factory.get_config_cell_serial().await?.to_vec(),
+    )?;
+    let libs = factory
+        .library_provider()
+        .get_libs_dict(&[code_cell, data_cell])
+        .await?;
+
+    let mut sums: ((Duration, Duration, Duration, Duration), Duration) = (
+        (
+            Default::default(),
+            Default::default(),
+            Default::default(),
+            Default::default(),
+        ),
+        Default::default(),
+    );
+
+    const MAX_ITER: usize = 10;
+    for i in 0..MAX_ITER {
+        let run_result: (
+            (TvmSuccess, Duration, Duration, Duration, Duration),
+            Duration,
+        ) = unsafe {
+            // Using unsafe to extend lifetime of references to method_id & stack.
+            //
+            // This is necessary because the compiler doesn't have a proof that these references
+            // outlive spawned future.
+            // But we're know it for sure since we're awaiting it. In normal async/await block
+            // this would be checked by the compiler, but not when using `spawn_blocking`
+            let static_method_id: TonMethodId = "get_pool_data".into();
+            let static_stack: &'static [TvmStackEntry] = std::mem::transmute(stack_ref);
+
+            let code = code.clone();
+            let data = data.clone();
+            let c7 = c7.clone();
+            let libs = libs.dict_boc.clone();
+
+            #[allow(clippy::let_and_return)]
+            let ovetall_t = Instant::now();
+            let res = tokio::task::spawn_blocking(move || {
+                let code = code.as_slice();
+                let data = data.as_slice();
+
+                let t_creation = Instant::now();
+                let emulator = TvmEmulator::new(code, data).unwrap();
+                let creation_time = t_creation.elapsed();
+
+                let t_c7 = Instant::now();
+                let e = emulator.with_c7(&c7).unwrap();
+                let c7_time = t_c7.elapsed();
+
+                let t_lib = Instant::now();
+                let mut e = e.with_libraries(libs.as_slice()).unwrap();
+                let lib_time = t_lib.elapsed();
+
+                let running_time = Instant::now();
+                let run_result = e.run_get_method(&static_method_id, static_stack);
+                let running_time = running_time.elapsed();
+                (
+                    run_result.unwrap(),
+                    creation_time,
+                    c7_time,
+                    lib_time,
+                    running_time,
+                )
+            })
+            .await
+            .map_err(|e| TonContractError::InternalError(e.to_string()))?;
+
+            let sum_t = ovetall_t.elapsed();
+            (res, sum_t)
+        };
+
+        log::info!("{} of {}: creation_time: {:?}, c7_time: {:?}, lib_time: {:?}, running_time: {:?}, overall+tokio: {:?}", i, MAX_ITER,run_result.0.1,run_result.0.2,run_result.0.3,run_result.0.4,run_result.1);
+
+        sums.0 .0 += run_result.0 .1;
+        sums.0 .1 += run_result.0 .2;
+        sums.0 .2 += run_result.0 .3;
+        sums.0 .3 += run_result.0 .4;
+        sums.1 += run_result.1
+    }
+
+    log::info!(
+        "_________________OVERALL over {}_________________",
+        MAX_ITER
+    );
+    log::info!("creation_time: {:?}, c7_time: {:?}, lib_time: {:?}, running_time: {:?}, overall+tokio: {:?}", sums.0.0, sums.0.1,sums.0.2,sums.0.3,sums.1);
+
     Ok(())
 }
